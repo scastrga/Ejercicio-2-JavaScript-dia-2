e81323dc241566f20bbb8ecde5c664f830f281c2
scastrga <scastrga@everis.com>
2021-04-15 12:04:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vgzrrlejercicio2-zpdyvv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vgzrrlejercicio2-zpdyvv)
---
+++ index.js
@@ -0,0 +1,3 @@
+// Crear un array de 3 objetos
+// Cada objeto es una persona que tendrá las propiedades edad, nombre y nacionalidad
+// Mostrar la edad del segundo elemento del array y la edad del ultimo elemento del array
