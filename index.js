262a4a53eb5bd9219b7477934c9785d5cb86366f
scastrga <scastrga@everis.com>
2021-04-15 12:05:07 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // Crear un array de 3 objetos
 // Cada objeto es una persona que tendrá las propiedades edad, nombre y nacionalidad
 // Mostrar la edad del segundo elemento del array y la edad del ultimo elemento del array
+
+
+const arrayUsu = [   
+{ name: "Jurgen", country: "Alemania", age: 19, },   
+{ name: "Virgolini", country: "Italia", age: 21 }, 
+{ name: "Redzed", country: "Russia", age: 40 } ];  
+
+console.log(arrayUsu[1]);
+console.log(arrayUsu[arrayUsu.length - 1])
